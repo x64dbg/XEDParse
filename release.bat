7ff3441a115919673aea309ab158e7c42dda3ba1
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-11-04 06:57:01 +0100

    added release script

---
+++ release.bat
@@ -0,0 +1,13 @@
+@echo off
+set RELEASEDIR=.\release
+rmdir /S /Q %RELEASEDIR%
+mkdir %RELEASEDIR%
+mkdir %RELEASEDIR%\x32
+mkdir %RELEASEDIR%\x64
+
+copy src\XEDParse.h %RELEASEDIR%\XEDParse.h
+xcopy bindings %RELEASEDIR% /S /Y
+copy bin\x32\XEDParse.dll %RELEASEDIR%\x32\XEDParse.dll
+copy bin\x32\XEDParse.lib %RELEASEDIR%\XEDParse_x86.lib
+copy bin\x64\XEDParse.dll %RELEASEDIR%\x64\XEDParse.dll
+copy bin\x64\XEDParse.lib %RELEASEDIR%\XEDParse_x64.lib
